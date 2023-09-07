8153b59465264331f9c8c45192a6eb88c761f7b5
Stefano Miceli <stefano.miceli@ext.leroymerlin.it>
2023-09-07 15:07:48 +0200

    Initial commit

---
+++ src/main/java/com/twentyfive/twentyfivebio/TwentyfiveBioApplication.java
@@ -0,0 +1,13 @@
+package com.twentyfive.twentyfivebio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyfiveBioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwentyfiveBioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twentyfive/twentyfivebio/TwentyfiveBioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentyfive.twentyfivebio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyfiveBioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
